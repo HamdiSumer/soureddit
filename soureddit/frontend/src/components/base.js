8e74c1a9498bb694ee35a0a98e91c1f79a14d99a
tugruldonmez <cavslebron@hotmail.com>
2023-07-11 23:18:11 +0300

    folder structure

---
+++ soureddit/frontend/src/components/base.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './base.css'; // Import the base.css file
+
+const BaseTemplate = () => {
+  return (
+    <div className="base-template">
+      <header>
+        {/* Header content */}
+      </header>
+      <nav>
+        {/* Navigation menu */}
+      </nav>
+      <main>
+        {/* Main content area */}
+      </main>
+      <footer>
+        {/* Footer content */}
+      </footer>
+    </div>
+  );
+};
+
+export default BaseTemplate;
