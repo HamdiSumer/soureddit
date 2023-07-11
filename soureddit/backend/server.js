06da490f81cc2c1f415aec2737e5313588a0076c
HamdiSumer <hamdismer@hotmail.com>
2023-07-11 20:51:56 +0300

    backend and frontend has been created

---
+++ soureddit/backend/server.js
@@ -0,0 +1,15 @@
+// Dependencies
+
+const express = require("express");
+
+// Create Applications
+const app = express()
+
+// Routing
+
+app.get("/", (req, res) => {
+    res.json({hello:"world"});
+});
+
+// Start our server
+app.listen(3001);
