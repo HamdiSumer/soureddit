099edebeef7eaff4592423781c880ef0e881d1ee
tugruldonmez <cavslebron@hotmail.com>
2023-09-06 21:59:07 +0300

    header logolar bitmemiş hal

--- soureddit/frontend/src/components/Base/Base.js
+++ soureddit/frontend/src/components/Base/Base.js
@@ -6,7 +6,12 @@ import Auth from '../User/Auth';
 import SignUp from '../User/SignUp';
 import SubSelections from '../User/SubSelections';
 import logo from '../img/logo.png';
-import Posts from'../Posts/Post'
+import redditLogo from '../img/reddit-logo-16.png'
+import eksiLogo from '../img/eksi.png'
+import twitterLogo from '../img/twitter-logo-change-x-elon-musk-designboom-fb.jpg'
+import Posts from'../Posts/Post';
+import 'tailwindcss/tailwind.css'; // Import the Tailwind CSS styles
+
 
 // App elementini belirle
 Modal.setAppElement('#root'); // Varsayılan olarak root elementi
@@ -73,19 +78,33 @@ const BaseTemplate = React.memo(() => {
       <body className='base-template'>
         <div>
           <div className='header-template'>
+
+            {/*<form action=''>*/}
+            {/*  <input type='text' placeholder='Subredditleri "r/" şeklinde arayın ' className='header-search-template'></input>*/}
+            {/*  <button className='search-icon-button' type="submit">Search</button>*/}
+            {/*</form>*/}
+
             <a className='header-logo-template' href='#'>
             <img src={logo} alt="Logo" className='header-logo-template' />
               <span className='header-logo-version-template'> v.0.0</span>
             </a>
-
-            <form action=''>
-              <input type='text' placeholder='Subredditleri "r/" şeklinde arayın ' className='header-search-template'></input>
-              <button className='search-icon-button' type="submit">Search</button>
-            </form>
             <div>
+              
             {isLoggedIn ? (
-              <div>
-                <span className='header-login-template'>Hoş Geldiniz</span>
+              
+              <div className='navbar-contents'>
+                <div className='platform-switch'>
+                
+                <button type="button" class=" bg-orange hover:bg-[#fd4400ff]/90 focus:ring-4 focus:outline-none focus:ring-[#fd4400ff]/50 font-medium rounded-lg text-sm px-3.5 py-1.5 text-center inline-flex items-center dark:focus:ring-[#fd4400ff]/55 mx-2 transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+                    <img src={redditLogo} alt="Reddit Logo" class="w-6 h-6" viewBox="0 0 20 17" fill="currentColor"/>
+                </button>
+                <button type="button" class=" bg-eksi-yesili hover:bg-eksi-yesili/90 focus:ring-4 focus:outline-none focus:ring-eksi-yesili/50 font-medium rounded-lg text-sm px-3.5 py-1.5 text-center inline-flex items-center dark:focus:ring-eksi-yesili/55 mx-2 transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+                    <img src={eksiLogo} alt="Eksi Logo" class="w-6 h-6" viewBox="0 0 20 17" fill="currentColor"/>
+                </button>
+                <button type="button" class=" bg-black hover:bg-black/90 focus:ring-4 focus:outline-none focus:ring-black/50 font-medium rounded-lg text-sm px-3.5 py-1.5 text-center inline-flex items-center dark:focus:ring-black/55 mx-2 transition-all duration-300 cursor-pointer filter contrast-50 focus:contrast-100 hover:contrast-100">
+                    <img src={twitterLogo} alt="Eksi Logo" class="w-6 h-6" viewBox="0 0 20 17" fill="currentColor"/>
+                </button>
+                </div>
                 <button className='buttons' onClick={handleSubSelectionModalOpen}>
                   Subreddit Tercihlerini Güncelle
                 </button>
@@ -94,14 +113,14 @@ const BaseTemplate = React.memo(() => {
                 </button>
               </div>
             ) : (
-    <div>
-      <a className='header-login-template' href='#' onClick={handleLoginModalOpen}>
-        Giriş Yap
-      </a>
-      <a className='header-login-template' href='#' onClick={handleSignUpModalOpen}>
-        Kayıt Ol
-      </a>
-    </div>
+            <div className='navbar-contents'>
+              <button className='buttons' onClick={handleLoginModalOpen}>
+                Giriş Yap
+              </button>
+              <button className='buttons' onClick={handleSignUpModalOpen}>
+                Kayıt Ol
+              </button>
+            </div>
   )}
 
             </div>
--- soureddit/frontend/tailwind.config.js
+++ soureddit/frontend/tailwind.config.js
@@ -9,13 +9,16 @@ module.exports = {
     extend: {
       colors: {
         'regal-blue': '#243c5a',
-        'eksi-yesili':'#626a12',
-        'reddit-grisi':'#e8dfd6',
-        'reddit-fontcolor':'#2a210c',
-        '526421':'#526421',
+        'eksi-yesili':'#52a500ff',
+        'reddit-grisi':'#D8D8D8',
+        'reddit-fontcolor':'#87888aff',
+        'orange':'#fd4400ff',
+        'light-orange':'#ed7371ff',
+        'lighter-orange':'#FADBD8',
         'b5c276':'#b5c276',
         'cinnemon':'#7b3f00',
         'koyu-reddit-grisi':'#b0a8a0',
+        'header-bg':'#fcfefbff',
       },
       height: {
         custom_for_scrollbar:'480px',
